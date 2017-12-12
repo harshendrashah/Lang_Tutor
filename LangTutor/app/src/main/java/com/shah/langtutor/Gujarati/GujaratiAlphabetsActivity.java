3651d656a4a857719cce277871d516ec220134f6
Harshendra Shah <harshendrashah812@gmail.com>
2017-12-12 13:51:34 +0530

    Initial commit

---
+++ LangTutor/app/src/main/java/com/shah/langtutor/Gujarati/GujaratiAlphabetsActivity.java
@@ -0,0 +1,17 @@
+package com.shah.langtutor.Gujarati;
+
+import com.shah.langtutor.R;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+
+public class GujaratiAlphabetsActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.words_list);
+
+    }
+}
---
+++ LangTutor/app/src/main/java/com/shah/langtutor/Gujarati/GujaratiPhrasesActivity.java
@@ -0,0 +1,14 @@
+package com.shah.langtutor.Gujarati;
+
+import com.shah.langtutor.R;
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class GujaratiPhrasesActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.words_list);
+    }
+}
